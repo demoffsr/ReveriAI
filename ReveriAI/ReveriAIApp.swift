81d23854744f0839941471d597f3d4d8cececfd7
demoffsrmain <ustimdima2002@gmail.com>
2026-02-10 20:41:03 +0100

    Initial Commit

---
+++ ReveriAI/ReveriAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReveriAIApp.swift
+//  ReveriAI
+//
+//  Created by Dmitry Demidov on 10.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ReveriAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
